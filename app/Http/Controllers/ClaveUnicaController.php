57dc8b30d0c405e2fc33bc860f107445ba50d95c
AquaroTorres <alvarotorres@gmail.com>
2020-03-31 02:19:32 -0300

    url base nueva

--- app/Http/Controllers/ClaveUnicaController.php
+++ app/Http/Controllers/ClaveUnicaController.php
@@ -4,14 +4,14 @@ use Illuminate\Http\Request;
 class ClaveUnicaController extends Controller
 {
     public function autenticar(){
-        $url_base = "https://accounts.claveunica.gob.cl/accounts/login/?next=/openid/authorize";
+        $url_base = "https://accounts.claveunica.gob.cl/openid/authorize";
         $client_id = 'a4b81d3aa23c457998312c0a980ebc4f';
         $redirect_uri = urlencode('https://i.saludiquique.cl/claveunica/callback');
         $state = csrf_token();
         $scope = 'openid+run+name';
         $url=$url_base.urlencode('?client_id='.$client_id.'&redirect_uri='.$redirect_uri.'&scope='.$scope.'&response_type=code&state='.$state);
         header("Location:$url");
-        die($url);
+        //die($url);
     }
 
     public function callback(Request $request) {
