aaf270508ce2e88f0bfd245da4e164cf2a5fd33a
AquaroTorres <alvarotorres@gmail.com>
2020-04-02 01:40:07 -0300

    probar argumento

--- app/Http/Controllers/ClaveUnicaController.php
+++ app/Http/Controllers/ClaveUnicaController.php
@@ -1,19 +1,22 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+
 class ClaveUnicaController extends Controller
 {
     public function autenticar(){
         $url_base = "https://accounts.claveunica.gob.cl/accounts/login/?next=/openid/authorize";
         $client_id = env("CLAVEUNICA_CLIENT_ID");
         $redirect_uri = urlencode(env("CLAVEUNICA_CALLBACK"));
-        $state = csrf_token();
+        $state = base64_encode(csrf_token().'/monitor/report');
         $scope = 'openid+run+name+email';
+
         $url=$url_base.urlencode('?client_id='.$client_id.'&redirect_uri='.$redirect_uri.'&scope='.$scope.'&response_type=code&state='.$state);
+
         return redirect()->to($url)->send();
-        // header("Location:$url");
-        // die($url);
     }
 
     public function callback(Request $request) {
@@ -24,7 +27,7 @@ class ClaveUnicaController extends Controller
         $client_id = env("CLAVEUNICA_CLIENT_ID");
         $client_secret = env("CLAVEUNICA_SECRET_ID");
         $redirect_uri = urlencode(env("CLAVEUNICA_CALLBACK"));
-        $state = csrf_token();
+        //$state = csrf_token();
         $scope = 'openid+run+name+email';
 
         $response = Http::asForm()->post($url_base, [
@@ -42,6 +45,7 @@ class ClaveUnicaController extends Controller
         $user_cu = json_decode($response);
 
         echo '<pre>';
+        echo substr(base64_decode($state), 40);
         print_r($user_cu);
         echo '</pre>';
     }
